931fdce42fc7a583f14e1185d3aa01376dbbfec8
amirgal <amirc538@gmail.com>
2020-03-14 00:33:36 +0200

    changed to functional components

--- src/components/Transaction.js
+++ src/components/Transaction.js
@@ -1,24 +1,24 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {Button} from '@material-ui/core'
 
-class Transaction extends Component {
 
-    deleteTransaction = () => {
-        this.props.deleteTransaction(this.props.transaction._id)
-    }
+function Transaction(props) {
 
-    render() {
-        const t = this.props.transaction
-        const operation = t.amount > 0 ? 'deposit' : 'withdrawal'
-        return (
-            <div className={`transaction ${operation}`} >
-                <p className="amount">Amount: {t.amount}</p>
-                <p className="vendor">Vendor: {t.vendor}</p>
-                <p className="category">Category: {t.category}</p>
-                <Button variant="contained" className="delete-btn" onClick={this.deleteTransaction}>Delete</Button>
-            </div>
-        )
+    const deleteTransaction = () => {
+        props.deleteTransaction(props.transaction._id)
     }
+    const t = props.transaction
+    const operation = t.amount > 0 ? 'deposit' : 'withdrawal'
+  
+
+    return (
+        <div className={`transaction ${operation}`} >
+            <p className="amount">Amount: {t.amount}</p>
+            <p className="vendor">Vendor: {t.vendor}</p>
+            <p className="category">Category: {t.category}</p>
+            <Button variant="contained" className="delete-btn" onClick={deleteTransaction}>Delete</Button>
+        </div>
+    ) 
 }
 
 export default Transaction
--- src/components/Transactions.js
+++ src/components/Transactions.js
@@ -1,27 +1,26 @@
-import React, {Component} from 'react';
+import React from 'react';
 import {Transaction} from './';
 import {Grid, Paper} from '@material-ui/core'
 
-class Transactions extends Component {
+function Transactions(props) {
 
-    deleteTransaction = (id) => {
-        this.props.deleteTransaction(id)
+    const deleteTransaction = (id) => {
+        props.deleteTransaction(id)
     }
 
-    render() {
-        return (
-            <div id="transactions">
-                <h3>Transactions</h3><hr/>
-                <Grid container direction="column" spacing={1} alignItems="center">
-                {this.props.transactions.map(t => 
-                    <Grid item xs={8} className="transItem" key={t._id}>
-                    <Paper><Transaction  transaction={t} deleteTransaction={this.deleteTransaction}/></Paper>
-                    </Grid>
-                    )}
+    return (
+        <div id="transactions">
+            <h3>Transactions</h3><hr/>
+            <Grid container direction="column" spacing={1} alignItems="center">
+            {props.transactions.map(t => 
+                <Grid item xs={8} className="transItem" key={t._id}>
+                    <Paper><Transaction  transaction={t} deleteTransaction={deleteTransaction}/></Paper>
                 </Grid>
-            </div>
-        )
-    }
+                )}
+            </Grid>
+        </div>
+    )
 }
+
 
 export default Transactions
